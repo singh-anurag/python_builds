09a606ce4395d30bc509003d66588ae5ff5c8a56
anurag singh <anuragsingh2112@gmail.com>
2021-05-14 01:30:38 +0530

    Initial commit

---
+++ test_code.py
@@ -0,0 +1,19 @@
+a = 3
+b = 8
+
+# perform addition
+c = a + b
+print(c)
+
+# perform subtraction
+s = c - b
+print(s)
+
+# perform multiplication
+p = c * s
+print(p)
+
+# perform divide
+d = p/c
+print(d)
+
